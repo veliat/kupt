b7db40fa5599a3dc13e6e604329ff017982958b5
veliat <60612082+veliat@users.noreply.github.com>
2024-07-25 00:43:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const staticCacheName = 's-app-v1'
+
+const assetUrls = [
+    'index.html',
+    '/css/style.css'
+
+]
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName)
+    await cache.addAll(assetUrls)
+  })
+
+self.addEventListener('activate', event => {
+    console.log('[SW]: activate')
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch',event.request)
+    event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request){
+    const cached = await caches.match(request)
+    return cached ?? await fetch(request)
+}
